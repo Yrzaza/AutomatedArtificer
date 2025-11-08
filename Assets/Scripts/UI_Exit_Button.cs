2532b2204c284673632387b2b05768f4668fdc5c
Yrzaza <Yearzedttv@gmail.com>
2025-11-07 19:26:55 -0600

    ReloadScene implemented

--- Assets/Scripts/UI_Exit_Button.cs
+++ Assets/Scripts/UI_Exit_Button.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class UI_Exit_Button : MonoBehaviour
 {
 
-        public void buttonExitGame()
-        {
-            // quit the fuckin game
-            Application.Quit();
+    public void buttonExitGame()
+    {
+        // quit the fuckin game
+        Application.Quit();
 
-            //make sure game is quit
-            Debug.Log("We leavin the game");
+        //make sure game is quit
+        Debug.Log("We leavin the game");
 
         }
-    
+    public void RestartGame()
+    {
+        //reload the scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //make sure game is reload
+        Debug.Log("We reloadin");
+    }
+
 }
